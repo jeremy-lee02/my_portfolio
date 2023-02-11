e4a3dc81ec42edbae5f258756125796c2212e399
Trung Tín <s3752975@rmit.edu.vn>
2023-02-11 15:06:55 +0700

    udpate

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,14 +20,14 @@ function Projects({projects}: Props) {
         <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin hover:scrollbar-thumb-[#00000065] scrollbar-thumb-[#0000ffb6] scrollbar-track-[#ffffffa4] scrollbar-thumb scrollbar-corner-white scrollbar-track'>
             {projects?.map((project , i) => (
                 <>
-                    <div className='flex flex-col mt-10'>
+                    <div className='flex flex-col mt-10' key={project._id}>
                         <h4 className='text-3xl font-semibold text-center'>
                                 <span className='underline decoration-[#00d5ff]'>Project {i + 1} of {projects.length}:</span>
                                 <Link href={project.url}>
                                     <span className='cursor-pointer hover:text-[#00d5ff]'> {project.projectName}</span> 
                                 </Link>
                         </h4>
-                        <div key={project._id} className='w-screen snap-center flex md:flex-row flex-col space-y-5 items-center justify-center px-16 h-full'>
+                        <div className='w-screen snap-center flex md:flex-row flex-col space-y-5 items-center justify-center px-16 h-full'>
                             <img src= {urlFor(project.skillImage).url()} width={300} height={370} alt=""  className='max-w-full' />
                             <div className='px-0 md:px-8 max-w-2xl h-1/2 overflow-y-auto'>
                                 <p className='text-[18px]'>{project.description}</p>
