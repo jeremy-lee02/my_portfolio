a83ad478b70f91d46c3eed5b28d3c20bfe0a284c
Trung Tín <s3752975@rmit.edu.vn>
2023-02-11 15:01:18 +0700

    update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -34,24 +34,6 @@ function Projects({projects}: Props) {
                         </div>
                     </div>
             ))}
-                        {/* {projects?.map((project , i) => (
-                    <div className='grid grid-row-2 mt-10'>
-                        <div>
-                            <h4 className='text-3xl font-semibold text-center'>
-                                    <span className='underline decoration-[#00d5ff]'>Project {i + 1} of {projects.length}:</span>
-                                    <Link href={project.url}>
-                                        <span className='cursor-pointer hover:text-[#00d5ff]'> {project.projectName}</span> 
-                                    </Link>
-                            </h4>
-                        </div>
-                        <div key={project._id} className='w-screen snap-center grid grid-cols-2 space-y-5 items-center justify-center px-16 h-full'>
-                            <img src= {urlFor(project.skillImage).url()} width={300} height={370} alt=""  className='max-w-full' />
-                            <div className='grid px-0 md:px-8 max-w-full h-1/2 overflow-y-auto'>
-                                <p className='text-[18px]'>{project.description}</p>
-                            </div>
-                        </div>
-                    </div>
-            ))} */}
         </div>
         <div className='w-full absolute top-[30%] bg-[#00d5ff34] left-0 h-[500px] -skew-y-12 ' />
         
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -24,7 +24,6 @@ export default function contact({result}:Props) {
       }}>
         <h3 className='uppercase tracking-[20px] text-2xl text-center pt-10'>Contact</h3>
         <div className='flex flex-col items-center justify-center md:mt-[100px] mt-[70px] flex-wrap'>
-          {/* <h4 className='text-4xl font-semibold text-center text-white'>Contact me for any information.</h4> */}
           <Contact info={result} /> 
         </div>
     </motion.div>
